8afe668519f3392cbe52760ef2516e870d9ffaaa
JUNYOUNG MOON <mjy0750@gmail.com>
2022-03-16 00:10:47 +0900

    Initial commit

---
+++ BookManager/Form4.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace BookManager
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxUserList = new System.Windows.Forms.ComboBox();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxUserList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "사용자 검색";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridViewBooks);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(888, 282);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "사용자 대출 도서 목록";
+            // 
+            // comboBoxUserList
+            // 
+            this.comboBoxUserList.FormattingEnabled = true;
+            this.comboBoxUserList.Location = new System.Drawing.Point(34, 41);
+            this.comboBoxUserList.Name = "comboBoxUserList";
+            this.comboBoxUserList.Size = new System.Drawing.Size(172, 20);
+            this.comboBoxUserList.TabIndex = 0;
+            this.comboBoxUserList.Text = "선택";
+            this.comboBoxUserList.SelectedIndexChanged += new System.EventHandler(this.selectedIndexChanged);
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(6, 20);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.RowTemplate.Height = 23;
+            this.dataGridViewBooks.Size = new System.Drawing.Size(876, 256);
+            this.dataGridViewBooks.TabIndex = 0;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 408);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form4";
+            this.Text = "사용자별 도서 대출 현황";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxUserList;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
+    }
+}
---
+++ BookManager/Form4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManager
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+
+            foreach (var item in DataManager.Users)
+            {
+                comboBoxUserList.Items.Add(item.Id.ToString());
+            }
+        }
+
+        private void selectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+
+            List<Book> output = (from item in DataManager.Books
+                         where (item.UserId.ToString() == cb.SelectedItem.ToString()
+                            && item.isBorrowed)
+                         select item).ToList();
+
+            dataGridViewBooks.DataSource = null;
+            dataGridViewBooks.DataSource = output;
+        }
+    }
+}
